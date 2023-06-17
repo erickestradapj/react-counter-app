02831321e24fc1af20c36a679ce41401b0d6e3e5
Erick Estrada <erickestradapj@gmail.com>
2023-06-17 11:24:18 -0500

    feat: print variables in HTML

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -2,6 +2,7 @@ export const FirstApp = () => {
    return (
       <>
          <h1>Erick</h1>
+         {/* <code>{JSON.stringify(newMessage)}</code> */}
          <p>I'm subtitle</p>
       </>
    );
