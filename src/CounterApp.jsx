8c9d0faf6707cef7b1f02c2f3b00c28ce57fd641
Erick Estrada <erickestradapj@gmail.com>
2023-06-17 13:34:03 -0500

    feat: handleSubtract and handleReset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -5,8 +5,15 @@ export const CounterApp = ({ value }) => {
    const [counter, setCounter] = useState(value);
 
    const handleAdd = () => {
-      //    setCounter((c) => c + 1);
       setCounter(counter + 1);
+   };
+
+   const handleSubtract = () => {
+      setCounter(counter - 1);
+   };
+
+   const handleReset = () => {
+      setCounter(value);
    };
 
    return (
@@ -15,6 +22,8 @@ export const CounterApp = ({ value }) => {
          <h2> {counter} </h2>
 
          <button onClick={handleAdd}>+1</button>
+         <button onClick={handleSubtract}>-1</button>
+         <button onClick={handleReset}>Reset</button>
       </>
    );
 };
