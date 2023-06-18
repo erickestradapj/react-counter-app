527e776c8c5bff4330f33f9a87bf89f7a2656881
Erick Estrada <erickestradapj@gmail.com>
2023-06-18 15:42:09 -0500

    chore: add eslint-plugin-jest

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,5 +1,10 @@
 test('this tests must not fail', () => {
-   if (0 === 1) {
-      throw new Error('Cannot be divide by 0');
-   }
+   // 1. Initialization
+   const message1 = 'Hello World ';
+
+   // 2.
+   const message2 = message1.trim();
+
+   // 3.
+   expect(message1).toBe(message2);
 });
