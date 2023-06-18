5a0cf7312f0d6e3ad5e22aba98261e4f4f32431d
Erick Estrada <erickestradapj@gmail.com>
2023-06-18 17:07:37 -0500

    feat: test in 07-destructuring-array.js

--- src/base-tests/06-destructuring-object.js
+++ src/base-tests/06-destructuring-object.js
@@ -1,20 +1,4 @@
-// Destructuring
-const person = {
-   name: 'Tony',
-   age: 45,
-   key: 'Ironman',
-   range: 'soldier',
-};
-
-// const { name, age, key } = person;
-
-// console.log(name);
-// console.log(age);
-// console.log(key);
-
 const usesContext = ({ name, age, key, range = 'captain' }) => {
-   // console.log(name, age, key, range);
-
    return {
       keyName: key,
       years: age,
@@ -24,11 +8,3 @@ const usesContext = ({ name, age, key, range = 'captain' }) => {
       },
    };
 };
-
-const {
-   keyName,
-   years,
-   latlng: { lat, lng },
-} = usesContext(person);
-console.log(keyName, years);
-console.log(lat, lng);
--- src/base-tests/07-destructuring-array.js
+++ src/base-tests/07-destructuring-array.js
@@ -1,26 +1,3 @@
-const characters = ['Goku', 'Vegeta', 'Trunks'];
-
-const [, , p3] = characters;
-
-console.log(p3);
-
-const returnArray = () => {
+export const returnArray = () => {
    return ['ABC', 123];
 };
-
-const [letters, numbers] = returnArray();
-console.log(letters, numbers);
-
-// Work
-const usState = (value) => {
-   return [
-      value,
-      () => {
-         console.log('Hello World');
-      },
-   ];
-};
-
-const [name, setName] = usState('Goku');
-console.log(name);
-setName();
