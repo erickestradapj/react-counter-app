164753aaeba44a20a95b8d6cccde9fdac1b119bc
Erick Estrada <erickestradapj@gmail.com>
2023-06-18 17:34:31 -0500

    feat: 08-import-export.js

--- src/base-tests/08-import-export.js
+++ src/base-tests/08-import-export.js
@@ -1,15 +1,9 @@
-import heroes, { owners } from '../data/heroes';
-
-// console.log(owners);
+import heroes from '../data/heroes';
 
 export const getHeroById = (id) => {
    return heroes.find((hero) => hero.id === id);
 };
 
-// console.log(getHeroById(2));
-
-const getHeroByOwner = (owner) => {
+export const getHeroByOwner = (owner) => {
    return heroes.filter((hero) => hero.owner === owner);
 };
-
-// console.log(getHeroByOwner('DC'));
