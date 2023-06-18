09e10c7d75515ee3f0174342971f05dcbdf7f014
Erick Estrada <erickestradapj@gmail.com>
2023-06-18 15:50:28 -0500

    feat: simple test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,10 +1,12 @@
-test('this tests must not fail', () => {
-   // 1. Initialization
-   const message1 = 'Hello World ';
+describe('Tests in <DemoComponent/>', () => {
+   test('this test must not fail', () => {
+      // 1. Arrange
+      const message1 = 'Hello World';
 
-   // 2.
-   const message2 = message1.trim();
+      // 2. Act
+      const message2 = message1.trim();
 
-   // 3.
-   expect(message1).toBe(message2);
+      // 3. Assert
+      expect(message1).toBe(message2);
+   });
 });
