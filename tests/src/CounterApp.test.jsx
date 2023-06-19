f90b1d991454b9bc7bbe649427bb3202a87f0ba2
Erick Estrada <erickestradapj@gmail.com>
2023-06-18 21:25:15 -0500

    feat: click event - testing

--- tests/src/CounterApp.test.jsx
+++ tests/src/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { CounterApp } from '../../src/CounterApp';
 
 describe('Test in <CounterApp />', () => {
@@ -12,5 +12,28 @@ describe('Test in <CounterApp />', () => {
    test('should show the initial value equal to 100', () => {
       render(<CounterApp value={100} />);
       expect(screen.getByText(100)).toBeTruthy();
+   });
+
+   test('should increase with the button +1', () => {
+      render(<CounterApp value={initialValue} />);
+      fireEvent.click(screen.getByText('+1'));
+      expect(screen.getByText('11')).toBeTruthy();
+   });
+
+   test('should decrease with the button -1', () => {
+      render(<CounterApp value={initialValue} />);
+      fireEvent.click(screen.getByText('-1'));
+      expect(screen.getByText('9')).toBeTruthy();
+   });
+
+   test('should work the reset button', () => {
+      render(<CounterApp value={355} />);
+      fireEvent.click(screen.getByText('+1'));
+      fireEvent.click(screen.getByText('+1'));
+      fireEvent.click(screen.getByText('+1'));
+      //   fireEvent.click(screen.getByText('Reset'));
+      fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }));
+
+      expect(screen.getByText(355)).toBeTruthy();
    });
 });
